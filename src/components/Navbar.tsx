07b30ef76988b9783bacab1281c592247d8c07b2
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2023-03-05 17:52:58 +0200

    Project setup

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import { Container, Navbar as NavBar } from "react-bootstrap";
+
+export const Navbar = () => {
+  return (
+    <NavBar className="bg-white shadow-sm mb-3">
+      <Container>Nav</Container>
+    </NavBar>
+  );
+};
