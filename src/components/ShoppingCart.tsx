661fee7145d1dd98e940ceebc276220f3c6d68f6
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2023-03-10 23:04:02 +0200

    Add total price in Cart

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,6 +1,8 @@
 import { Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
+import { formatCurrency } from "../utilities/formatCurrency";
 import { CartItem } from "./CartItem";
+import storeItems from "../data/data.json";
 
 type ShoppingCartProps = {
   isOpen: boolean;
@@ -19,6 +21,18 @@ export const ShoppingCart = ({ isOpen }: ShoppingCartProps) => {
           {cartItems.map((item) => (
             <CartItem key={item.id} {...item} />
           ))}
+          <div className="ms-auto fw-bold fs-5">
+            Total
+            {formatCurrency(
+              cartItems.reduce((total, cartItem) => {
+                const item = storeItems.find((i) => i.id === cartItem.id);
+
+                return (
+                  total + ((item?.price as number) || 0) * cartItem.quantity
+                );
+              }, 0)
+            )}
+          </div>
         </Stack>
       </Offcanvas.Body>
     </Offcanvas>
